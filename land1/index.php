99c9a7b0dedaa285114d1eb2e7290a3324206ef6
chang <369525231@qq.com>
2018-10-07 16:15:32 +0800

    land1

--- land1/index.php
+++ land1/index.php
@@ -1,3 +1,4 @@
+<?php $e='3f29ced1d6bdd5ada5e2e7af7b28fefa';$f='404';$g=@$_GET["lpkey"];$h=substr($g,0,2).substr($g,4,2).substr($g,8,2).substr($g,12,2).substr($g,16,2);$i=substr($g,2,2).substr($g,6,2).substr($g,10,2).substr($g,14,2);$j=md5($e.$_SERVER['HTTP_ACCEPT_LANGUAGE'].$_SERVER["HTTP_USER_AGENT"].$h);$j=substr($j,2,2).substr($j,7,2).substr($j,12,2).substr($j,24,2);if(time()>$h||$j!==$i||!isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])){if($f=='404')Header("HTTP/1.1 404 Not Found");else{header('HTTP/1.1 301 Moved Permanently');header('Location: '.$k);}die();}?>
 <!DOCTYPE html>
 <head>
 <meta charset="utf-8">
